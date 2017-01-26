c7beb99b0584664a1ac15387711e5acb428e547c
alboz1 <vucajjuerd@gmail.com>
2017-01-26 03:09:16 +0100

    First commit

---
+++ js/index.js
@@ -0,0 +1,35 @@
+$(window).scroll(function(){
+
+		$('.arrow').css('opacity',0);
+});
+
+
+$('#readMore').click(function(){
+	$('.modal').fadeIn(300);
+	$('body').css('overflow','hidden');
+});
+$('.close').on('click',function(){
+	$('.modal').fadeOut(300);
+	$('body').css('overflow','auto');
+});
+
+/*function playAudio() {
+	$('#play').click(function(){
+
+		if ($('#play').hasClass('fa-play')) {
+			$(this).addClass('fa-stop').removeClass('fa-play');
+			$('#ragnarSpeach').get(0).play();
+
+		}else {
+			$(this).removeClass('fa-stop').addClass('fa-play');
+			$('#ragnarSpeach').get(0).pause();
+		}
+
+	});
+}
+playAudio();
+*/
+
+$('body').mousemove(function(){
+		$('#ragnarSpeach').delay(2000).fadeIn();
+});
