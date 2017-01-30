61f4a73cb07ff784098206f270bcd1d8c2a99c63
alboz1 <vucajjuerd@gmail.com>
2017-01-30 03:00:54 +0100

    WIP

--- js/index.js
+++ js/index.js
@@ -1,17 +1,37 @@
-$(window).scroll(function(){
-
+$(document).ready(function(){
+	$(window).scroll(function(){
 		$('.arrow').css({'opacity':0,'animation': 'none'});
+	});
+	$('body').mousemove(function(){
+		$('#ragnarSpeach').delay(2000).fadeIn();
+	});
+
+	openCloseModal();
+	ragnarssons();
 });
+function openCloseModal(){
+
+	$('#readMore').click(function(){
+		$('.modal').fadeIn(300);
+		$('body').css('overflow','hidden');
+	});
+
+	$('.close').on('click',function(){
+		$('.modal').fadeOut(300);
+		$('body').css('overflow','auto');
+	});
+}
+function ragnarssons () {
+	$('li:last-child').find('.arrow-up .about-son').css('left','-'+ 295+ 'px');
+
+	$('li').click(function() {
+		var $this = $(this);
+		$this.find('.arrow-up').fadeIn(300);
+		$this.siblings().find('.arrow-up').fadeOut(300);
+	});
 
 
-$('#readMore').click(function(){
-	$('.modal').fadeIn(300);
-	$('body').css('overflow','hidden');
-});
-$('.close').on('click',function(){
-	$('.modal').fadeOut(300);
-	$('body').css('overflow','auto');
-});
+}
 
 /*function playAudio() {
 	$('#play').click(function(){
@@ -29,9 +49,3 @@ $('.close').on('click',function(){
 }
 playAudio();
 */
-
-$('body').mousemove(function(){
-
-			$('#ragnarSpeach').delay(2000).fadeIn();
-
-});
